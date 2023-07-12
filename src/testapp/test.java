d7688c440a1f40170cc966e62ae907462862499c
dharmawip <137141110+dharmawip@users.noreply.github.com>
2023-07-12 12:05:04 +0530

    Add files via upload

---
+++ src/testapp/test.java
@@ -0,0 +1,42 @@
+<<<<<<< HEAD
+package com.mycompany.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+=======
+package com.mycompany.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+>>>>>>> 5fca77bac6ce11567ee30f02b9334bd3b7b0dcfe
+}
